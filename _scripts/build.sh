2bbe309ec3c7cabf1d9d3a6c548b3df81b0fc468
ukuq <ukuq@qq.com>
2026-04-19 20:40:40 +0800

    opt: build script

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,2 +1,9 @@
-compc -target-player='32.0' -source-path src -library-path lib/as3corelib.swc -library-path lib/greensock-taomee.swc -library-path lib/textLayout.swc -output out/Seer2CoreDLL.swc -include-sources src -warnings=false
-
+compc \
+  -target-player 32 \
+  -source-path ../src \
+  -library-path ../lib/as3corelib.swc \
+  -library-path ../lib/greensock-taomee.swc \
+  -library-path ../lib/textLayout.swc \
+  -output ../out/Seer2CoreDLL.swc \
+  -include-sources ../src \
+  -warnings=false
---
+++ _scripts/build2-copy-core-dll.sh
@@ -0,0 +1,7 @@
+./build.sh
+./build1-seer2-core.dll.sh
+
+DLL_FILE="../out/Seer2CoreDLL.swf"
+OUTPUT_FILE="/e/sync/seer2/dll/Seer2CoreDLL.swf"
+
+cp $DLL_FILE $OUTPUT_FILE
